d6fe80a75f389479395c9b9d022d60b059122f31
gjrocky <gjrocky22@gmail.com>
2024-03-26 21:20:12 -0400

    adding typewriter effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { TypeAnimation } from 'react-type-animation';
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-[#fefae0] text-3xl font-mono text-[14px] text-[#161a1d] flex items-center justify-center h-screen">
+      <button>
+      <TypeAnimation
+          sequence={[
+            'yurbys_art_gallery', // Types 'One'
+            2000, // Waits 2s
+            'login/create.account?',
+            () => {
+              console.log('Sequence completed');
+            },
+          ]}
+          wrapper="span"
+          cursor={true}
+          repeat={0}
+          style={{ fontSize: '2em', display: 'inline-block' }}
+          speed={25}
+        />
+      </button>
+    </div>
   )
 }
 
